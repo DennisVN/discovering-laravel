fb5b638cb7c9b82a7f3212522befc9e3613ed88f
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-08-24 14:46:57 +0200

    Remove Laravel homepage, switch with basic HTML structure and use as HomePage

---
+++ resources/views/form.blade.php
@@ -0,0 +1,14 @@
+@section('form')
+
+<h2>✈️ If you ain't got no money take yo' broke ass home✈️ <br>
+    You say: If you ain't got no money take yo'broke ass home<br>
+    🥂G-L-A-M-O-R-O-U-S🥂<br> yeah G-L-A-M-O-R-O-U-S</h2><br>
+<form> 
+  <div class="form-group">
+    <label for="form">Who created this highly intelligent masterpiece ?</label>
+    <input type="text" class="form-control text-center" id="form" placeholder="Enter Fergie">
+    <small id="emailHelp" class="form-text text-muted">We'll never mock Fergie with anyone else.</small>
+  </div>
+</form>
+
+@endsection
